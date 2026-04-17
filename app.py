7b97d81d0aec6deb564380c04af3c0b5ad520ed0
TranNhuKhaY512 <y.2374802010582@vanlanguni.vn>
2026-04-17 09:46:49 +0700

    Update and rename app1.py to app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import google.generativeai as genai
 # ======================
 # GOOGLE GEMINI CONFIG
 # ======================
-GEMINI_API_KEY = "YOUR_API_KEY" 
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 gemini_model = genai.GenerativeModel("models/gemini-2.5-flash") 
 
